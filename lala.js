598e81a907d03cc5c25fa574e0019c0e0e592fe0
Marcos Manzino <marcosmanzino01@gmail.com>
2022-12-11 09:31:18 -0300

    lala

--- lala.js
+++ lala.js
@@ -1,4 +1,7 @@
-
+/*
+sistema de turnos para peliqueria  (ingresa con que profecional se atiende, en la consola muestra la lista de profecionales y el tiempo de espera entre corte y corte )
+si desea probar el codigo de forma rapida poner 1 o 2 de forma entercalada en los pront y tocar enter para que se llenen los parametros rapido 
+*/ 
 class cliente {
     constructor(id, nombre, precio, demora, profecional) {
         this.id  =(id);
@@ -15,7 +18,7 @@ let numPepe = 0
 const clientesPepe = [];
 const clientesJuan = [];
 
-for (let i= 0; i <= 4; i++) {
+for (let i= 0; i <= 9; i++) {
   numero = prompt("elegir profecional escribiendo 1 o 2 dependiendo su eleccion \n1. Pepe\n2. Juan")
     if (numero == 1) {
         alert("usted eligio a Pepe")
